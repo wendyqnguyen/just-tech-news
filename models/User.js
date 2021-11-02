8946cd5282a2b161598e4444c47e227c322f8dd5
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-11-02 13:42:47 -0700

    added login

--- models/User.js
+++ models/User.js
@@ -2,9 +2,13 @@ const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-
 // create our User model
-class User extends Model {};
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
 
 User.init(
     {
@@ -54,4 +58,5 @@ User.init(
     }
   );
 
+  
 module.exports = User;
